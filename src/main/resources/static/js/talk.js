b79686a9d757fed4919db91c4673f2d6e8608a8f
ZhsKevin <260133172@qq.com>
2021-04-30 20:44:37 +0800

    tips error and view

--- src/main/resources/static/js/talk.js
+++ src/main/resources/static/js/talk.js
@@ -5,7 +5,7 @@ function commentTalk(type,tagetId,content,ip,token) {
     if(content.length>1024)
         swal({
             title: "回复超过1024个字长!",
-            text: "你的回复字数为:"+content.length+"，请精简您的发言!",
+            text: "您的回复字数为:"+content.length+"，请精简您的发言!",
             icon: "warning",
             button: "确认",
         });
